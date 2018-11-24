e4d67c1acd7d90de99cc84a941562e44e4cbb666
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-11-24 23:54:43 +0700

    Single schema

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-const { buildSchema, GraphQLSchema } = require('graphql');
-const { Pool } = require('pg');
-const { mergeSchemas, makeExecutableSchema } = require('graphql-tools');
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const bodyParser = require("body-parser");
+const { buildSchema, GraphQLSchema } = require("graphql");
+const { Pool } = require("pg");
+const { graphql } = require("graphql");
+const { mergeSchemas, makeExecutableSchema } = require("graphql-tools");
 const {
   withPostGraphileContext,
   createPostGraphileSchema,
   createPostGraphQLSchema
-} = require('postgraphile');
+} = require("postgraphile");
 
-const { coursesData } = require('./data');
-const { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } = require('./dbconfig');
+const { coursesData } = require("./data");
+const { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } = require("./dbconfig");
 const pgConfig = new Pool({
   host: DB_HOST,
   database: DB_NAME,
@@ -48,32 +50,102 @@ const getCourses = args => {
 };
 
 const rootValue = {
-  message: () => 'Hello World!',
+  message: () => "Hello World!",
   course: getCourse,
-  courses: getCourses
+  courses: getCourses,
+  allPeople: () => "all people"
 };
 
 const createSchema = async () => {
-  const schema = await createPostGraphQLSchema(pgConfig, ['forum_example']);
-  console.log('[createSchema] > schema: ', schema);
+  const schema = await createPostGraphQLSchema(pgConfig, ["forum_example"]);
+  console.log("[createSchema] > schema: ", schema);
   // const s = new GraphQLSchema(schema);
   return schema;
 };
 
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
+
 const startServer = async () => {
   const graphileSchema = await createPostGraphQLSchema(pgConfig, [
-    'forum_example'
+    "forum_example"
   ]);
   // const graphileSchema = await createSchema();
   // console.log('[startServer] > mySchema: ', mySchema);
   // console.log('[startServer] > graphileSchema: ', graphileSchema);
-  const schema = mergeSchemas({ schemas: [graphileSchema, mySchema] });
+  const schema = mergeSchemas({
+    schemas: [graphileSchema, mySchema],
+    resolvers: {}
+  });
   const app = express();
-  app.use('/graphql', expressGraphQL({ schema, rootValue, graphiql: true }));
+  app.use(bodyParser.json());
+
+  const performQuery = async (
+    schema,
+    query,
+    variables,
+    jwtToken = null,
+    operationName
+  ) => {
+    return await withPostGraphileContext(
+      {
+        pgPool: pgConfig,
+        jwtToken,
+        jwtSecret: null,
+        pgDefaultRole: null
+      },
+      async context => {
+        // Execute your GraphQL query in this function with the provided
+        // `context` object, which should NOT be used outside of this
+        // function.
+        return await graphql(
+          schema, // The schema from `createPostGraphileSchema`
+          query,
+          null,
+          { ...context }, // You can add more to context if you like
+          variables,
+          operationName
+        );
+      }
+    );
+  };
+
+  let myContext = null;
+
+  withPostGraphileContext(
+    {
+      pgPool: pgConfig,
+      jwtToken: null,
+      jwtSecret: null,
+      pgDefaultRole: null
+    },
+    async context => {
+      await delay(3 * 1000);
+      myContext = context;
+    }
+  );
+
+  app.use((_, res, next) => {
+    if (!myContext) {
+      res.send("Waiting for myContext");
+      return;
+    }
+
+    next();
+  });
+
+  app.post("/graphql", async (req, res) => {
+    console.log("[Go into custom POST]");
+    const { query } = req.body;
+    const data = await performQuery(graphileSchema, query);
+    // const data = await performQuery(schema, query);
+    res.json(data);
+  });
+
+  app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
 
   app.listen(8080, () =>
     console.log(
-      'Express with GraphQL Server now is running on http://localhost:8080/graphql'
+      "Express with GraphQL Server now is running on http://localhost:8080/graphql"
     )
   );
 };
